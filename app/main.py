707bd4313bde43d7559020c3e326ae8cb311e0d6
avoytkiv <74664634+avoytkiv@users.noreply.github.com>
2023-07-14 22:10:39 +0000

    Squashed commit of the following:
    commit 6485b4e06bb7c5d768fe841940e6bf69361b173d
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 22:27:37 2023 +0000
    
        Duplicated deployment workflow for prod
    
    commit 5c62e9d04ebf2946fb3c83c189c146018e12e785
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 22:25:41 2023 +0000
    
        Corrected workflow
    
    commit f9a6566d2e31f6b2cf863123599367f14cda371b
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 22:05:21 2023 +0000
    
        Created a deploy workflow
    
    commit efe68882331375334b72a638d9342b6d2767603b
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 22:03:43 2023 +0000
    
        Created app scrypt
    
    commit 44db8b96cc6b3a53a7e9624ec07b06279d5406c3
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 21:56:53 2023 +0000
    
        Created a dockerfile for heroku
    
    commit 2eda0298cfc62f9bfe77912830984292ac443bbc
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 21:56:27 2023 +0000
    
        Created a procfile for heroku
    
    commit e155376d9dbf24c38df553ef681cecd1d8fc7a9f
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 21:24:30 2023 +0000
    
        Updated dvc piipeline with featurizer
    
    commit ff4a83f5a9ca2f47086adc4c715fba0eee29fa73
    Merge: 45d655b cd2a615
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 21:19:41 2023 +0000
    
        Merge branch 'fix/pca-model-save' into dev
    
    commit cd2a615413fb8020e8cbe9594b1c73aa091da6dc
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 21:13:23 2023 +0000
    
        Updated stages in dvc file
    
    commit 42f17e80994406319d03a3b1a88e8dea6209535a
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 21:08:35 2023 +0000
    
        Updated visualizing script to use featurizer
    
    commit 87f430e1529367b3ec6636d7d7636212877b0246
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 20:53:30 2023 +0000
    
        Save featurizer
    
    commit fd0c3d367c80385ba36be127e18fcfcf400502dc
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 20:53:12 2023 +0000
    
        Added path to params to save featurizer
    
    commit 45d655b44bda2ff0ce153d3cb91f2fd97c7b89ad
    Merge: b207e67 b644d14
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 20:28:39 2023 +0000
    
        Merge branch 'fix/experiments-workflow' into dev
    
    commit b644d14ad77766365bcf31f57d5e59bac29eee4f
    Author: avoytkiv <74664634+avoytkiv@users.noreply.github.com>
    Date:   Thu Jul 13 20:28:00 2023 +0000
    
        Fix: corrected branch name

---
+++ app/main.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+import uvicorn
+from fastapi import FastAPI
+import pandas as pd
+from joblib import load
+from pydantic import BaseModel
+from sklearn.preprocessing import StandardScaler
+
+# Defining the path to the model
+MODEL_JOBLIB_PATH = Path('models/model.pkl').absolute()
+PCA_JOBLIB_PATH = Path('models/featurizer.pkl').absolute()
+
+# Creating the App object
+app = FastAPI()
+
+# Using Pydantic lib, defining the data type for all the inputs from iris dataset
+class ModelInput(BaseModel):
+    '''
+    Defining the data type for all the inputs from processed dataset
+    '''
+    BALANCE: float
+    BALANCE_FREQUENCY: float
+    PURCHASES: float
+    ONEOFF_PURCHASES: float
+    INSTALLMENTS_PURCHASES: float
+    CASH_ADVANCE: float
+    PURCHASES_FREQUENCY: float
+    ONEOFF_PURCHASES_FREQUENCY: float
+    PURCHASES_INSTALLMENTS_FREQUENCY: float
+    CASH_ADVANCE_FREQUENCY: float
+    CASH_ADVANCE_TRX: float
+    PURCHASES_TRX: float
+    CREDIT_LIMIT: float
+    PAYMENTS: float
+    MINIMUM_PAYMENTS: float
+    PRC_FULL_PAYMENT: float
+    TENURE: float
+
+# Load the model from disk
+model = load(MODEL_JOBLIB_PATH)
+pca = load(PCA_JOBLIB_PATH)
+
+
+# Defining the prediction function
+def predict(data):
+    '''
+    Defining the prediction function
+    '''
+    data_df = pd.DataFrame([data])  # Convert input dictionary to DataFrame
+    data_featurized = pca.transform(StandardScaler().fit_transform(data_df)) # Featurize data
+    prediction = model.predict(data_featurized)[0]
+    return prediction
+
+
+# Defining the root endpoint of the API
+@app.get('/')
+def index():
+    '''
+    Defining the root endpoint of the API
+    '''
+    return {'message': 'Hello World'}
+
+# Defining the prediction endpoint of the API
+@app.post('/predict')
+def get_prediction(data: ModelInput):
+    '''
+    Defining the prediction endpoint of the API
+    '''
+    data = data.dict()
+    print(data)
+    data = pd.DataFrame([data])
+    print(data.head())
+
+    prediction = model.predict(data)[0]
+    return str(prediction)  
