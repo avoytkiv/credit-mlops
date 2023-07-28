efa0bc219743704dab2cd07e03fc32723e25f8e5
avoytkiv <74664634+avoytkiv@users.noreply.github.com>
2023-07-28 22:53:01 +0000

    Refactored training

--- src/stages/training.py
+++ src/stages/training.py
@@ -2,10 +2,9 @@ import argparse
 import json
 import joblib
 import pandas as pd
-from typing import Text
-import yaml
 from pathlib import Path
 import sys
+import dvc.api
 
 src_path = Path(__file__).parent.parent.parent.resolve()
 sys.path.append(str(src_path))
@@ -14,30 +13,26 @@ from src.utils.logs import get_logger  # noqa: E402
 from src.utils.train_util import cluster  # noqa: E402
 
 
-def train_model(config_path: Text) -> None:
-    """Train model.
-    Args:
-        config_path {Text}: path to config
-    """
+def train_model() -> None:
+    """Train model."""
 
-    with open(config_path) as conf_file:
-        config = yaml.safe_load(conf_file)
+    config = dvc.api.params_show()
 
     logger = get_logger("TRAIN", log_level=config["base"]["log_level"])
 
-    logger.info("Get estimator name")
-    estimator_name = config["train"]["estimator_name"]
-    logger.info(f"Estimator: {estimator_name}")
+    logger.info("Get model name")
+    model_name = config["train"]["model"]["model_name"]
+    logger.info(f"Model: {model_name}")
 
     logger.info("Load featurized dataset")
-    train_df = pd.read_csv(config["featurize"]["features_path"])
+    train_df = pd.read_csv(config["data"]["data_featurized"])
 
     logger.info("Train model")
 
     model = cluster(
         df=train_df,
-        estimator_name=estimator_name,
-        param_grid=config["train"]["estimators"][estimator_name]["param_grid"]
+        model_name=model_name,
+        param_grid=config["train"]["model"]["param_grid"]
     )
     logger.info(f"Best score: {model.best_score_}")
 
@@ -49,16 +44,11 @@ def train_model(config_path: Text) -> None:
     report = {"silhouette_score": str(model.best_score_)}
 
     logger.info("Save metrics")
-    metrics_path = Path(config["reports"]["reports_dir"]) / config["reports"]["metrics_file"]
+    metrics_path = Path(config["reports"]["base_dir"]) / config["reports"]["metrics_file"]
     json.dump(report, open(metrics_path, "w"))
     logger.info("Metrics saved")
 
 
 
 if __name__ == "__main__":
-
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument("--config", dest="config", required=True)
-    args = args_parser.parse_args()
-
-    train_model(config_path=args.config)
+    train_model()
