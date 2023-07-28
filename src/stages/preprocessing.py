b3719af8fdd1f09b8906875211e9897ece70f304
avoytkiv <74664634+avoytkiv@users.noreply.github.com>
2023-07-28 20:53:06 +0000

    Refactored preprocessing

--- src/stages/preprocessing.py
+++ src/stages/preprocessing.py
@@ -1,9 +1,7 @@
 import sys
-import argparse
-from typing import Text
-import yaml
 from pathlib import Path
 import pandas as pd
+import dvc.api
 
 src_path = Path(__file__).parent.parent.parent.resolve()
 sys.path.append(str(src_path))
@@ -23,19 +21,18 @@ def remove_outliers(df: pd.DataFrame) -> pd.DataFrame:
     return df_no_outliers
 
 
-def data_load(config_path: Text) -> None:
+def data_load() -> None:
     """Load raw data.
     Args:
         config_path {Text}: path to config
     """
 
-    with open(config_path) as conf_file:
-        config = yaml.safe_load(conf_file)
+    config = dvc.api.params_show()
 
     logger = get_logger("DATA_LOAD", log_level=config["base"]["log_level"])
 
     logger.info("Get dataset")
-    df = pd.read_csv(config["data_load"]["dataset_csv"])
+    df = pd.read_csv(config["data"]["data_load"])
     logger.info("Clean dataset")
     logger.info("Drop missing values")
     df = df.dropna()
@@ -44,14 +41,9 @@ def data_load(config_path: Text) -> None:
     logger.info("Remove outliers")
     df = remove_outliers(df)
     logger.info("Save raw data")
-    df.to_csv(config["data_preprocessing"]["dataset_processed"], index=False)
+    df.to_csv(config["data"]["data_preprocessing"], index=False)
 
 
 
 if __name__ == "__main__":
-
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument("--config", dest="config", required=True)
-    args = args_parser.parse_args()
-
-    data_load(config_path=args.config)
+    data_load()
