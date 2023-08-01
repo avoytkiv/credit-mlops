19ace3fc7bd4018a2bc504cfdc14952c0a1bb830
avoytkiv <74664634+avoytkiv@users.noreply.github.com>
2023-08-01 17:51:35 +0000

    Refactored visualization as seperate stage

--- src/stages/training.py
+++ src/stages/training.py
@@ -49,26 +49,12 @@ def train_model() -> None:
     json.dump(report, open(metrics_path, "w"))
     logger.info("Metrics saved")
 
-    # Append cluster labels to featurized dataset and save
+    logger.info("Save labeled dataset")
     train_df["cluster"] = labels
     train_df.to_csv(Path(config["data"]["data_labeled"]), index=False)
 
-    logger.info("Plot clusters")
-    # Create a scatter plot of the featurized data with different colors for each cluster
-    plt.figure(figsize=(8, 6))
-    sns.scatterplot(x=train_df.iloc[:, 0], y=train_df.iloc[:, 1], hue=labels, palette='viridis')
-    plt.title(f"Clusters for {model_name}")
-    plt.xlabel("Component 1")
-    plt.ylabel("Component 2")
-    plt.legend(title='Cluster Label', loc='upper right')
-    plt.show()
-
-    logger.info("Save plot")
-    plt.savefig(Path(config["reports"]["base_dir"]) / 
-                Path(config["reports"]["plots_dir"]) / 
-                Path(config["reports"]["plots"]["clusters"]))
-
-
+    
+   
 
 if __name__ == "__main__":
     train_model()
